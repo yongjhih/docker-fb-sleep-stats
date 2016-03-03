b9114885180c9d82fa18f618a077435605fdadef
Andrew Chen <yongjhih@gmail.com>
2016-03-04 06:19:48 +0800

    docker: npm run scrape

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,6 @@ PORT=${PORT:-3000}
 # rebuild config every time if necessary
 envsubst < "${FB_SLEEP_STATS_HOME}/config/default.json.example" > "${FB_SLEEP_STATS_HOME}/config/development.json"
 
+npm run scrape > /dev/null
+
 exec "$@"
