4ef1ba1e0b15bee9f70443691b24d0092d829d83
Andrew Chen <yongjhih@gmail.com>
2016-03-04 06:09:41 +0800

    Fix rm file error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
-
 POLLING_INTERVAL=${POLLING_INTERVAL:-600}
 APP_ID=${APP_ID:-435522656639081}
 PORT=${PORT:-3000}
 
-envsubst < "${FB_SLEEP_STATS_HOME}/config/default.json" > "${FB_SLEEP_STATS_HOME}/config/development.json"
-rm "${FB_SLEEP_STATS_HOME}/config/default.json" > /dev/null
+envsubst < "${FB_SLEEP_STATS_HOME}/config/default.json.example" > "${FB_SLEEP_STATS_HOME}/config/development.json"
 
 exec "$@"
